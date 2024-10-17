7f8951c50ec1574d8e0800933ea7eb5b789f3d4b
baish <baish@BAISHNABI>
2024-10-17 13:26:35 +0530

    Add project files.

---
+++ TupleToDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+public class TupleToDict
+{
+    static void Main(string[] args)
+    {
+        Tuple<int, string, int> tuple = new Tuple<int, string, int>(23,"hii",24);
+        Console.WriteLine("item1:"+tuple.Item1);
+        Console.WriteLine("Item2:" + tuple.Item2);
+        Console.WriteLine("Item3:" + tuple.Item3);
+        
+        Dictionary<int,string> dict = new Dictionary<int,string>();
+        dict.Add(tuple.Item1, tuple.Item2);
+        dict.Add(tuple.Item1, tuple.Item2);
+        foreach(var i in dict)
+        {
+            Console.WriteLine($"{i.Key}:{i.Value}");
+        }
+
+
+    }
+}
